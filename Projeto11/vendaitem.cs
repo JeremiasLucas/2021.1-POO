0fe98b0ac9066512c36dad1d76600a28ea890948
JeremiasLucas <88001432+JeremiasLucas@users.noreply.github.com>
2021-09-16 16:19:30 -0300

    Create vendaitem.cs

---
+++ Projeto11/vendaitem.cs
@@ -0,0 +1,35 @@
+using System;
+
+class VendaItem {
+  // Atributos do item de VendaPizza
+  private int porcoes;
+  private double preco;
+  // Associação entre VendaItem e pizza
+  private Pizza pizza;
+  public VendaItem(int porcoes, Pizza pizza) {
+    this.porcoes = porcoes;
+    this.preco = pizza.GetPreco();
+    this.pizza = pizza;
+  }
+  public void SetPorcoes(int porcoes) {
+    this.porcoes = porcoes;
+  }
+  public void SetPreco(double preco) {
+    this.preco = preco;
+  }
+  public void SetPizza(Pizza pizza) {
+    this.pizza = pizza;
+  }
+  public int GetPorcoes() {
+    return porcoes;
+  }
+  public double GetPreco() {
+    return preco;
+  }
+  public Pizza GetPizza() {
+    return pizza;
+  }
+  public override string ToString(){
+    return pizza.GetDescricao() + " - Porcoes: " + porcoes + " - Preco: " + preco.ToString("c2");
+  }
+}
